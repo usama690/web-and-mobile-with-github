2e25553b64be7c24bb999c7eb4f60aa35e756d46
usama690 <57412353+usama690@users.noreply.github.com>
2020-06-17 04:15:23 -0700

    assignment # 6

---
+++ javascript project/chapter 1/app.js
@@ -0,0 +1,12 @@
+// chapter #1 
+
+// question no 1
+alert("Welcome To My Website")
+// question no 2 
+alert("Error! Please enter a valid password.")
+// question no 3
+alert("Welcome to JS Land \n Happy Coding " ) 
+// question no 4 
+alert("Welcome to JS Land")
+alert("Happy Coding" )
+alert.confirm("prevent from using additional dialogs")
---
+++ javascript project/chapter 4/app.js
@@ -0,0 +1,27 @@
+// question no 1 
+a=b=c= "usama"
+// question no 2
+// legal variables  
+var $noone=""
+var _notwo=""
+var no3=""
+var nofour=""
+var no_five=""
+
+// illegal variables 
+
+// var -noone="";
+// var @notwo="";
+// var 3no="";
+// var !nofour="";
+// var ""nofive="";
+
+// question no 3 
+var heading = "Rules for naming JS variables"
+var heading2 = heading.bold()
+var a1= "variables names can only contain , numbers, $ and _. For example $my_1stVariable" 
+var a2= "variable must begin with a letter , $ or _ . For example : $name, _name  or name"
+var a3= "variables names are case sensitive"
+var a4= "variables names should not be JS keywords"
+document.write(heading2 + "<br> <br> <br>" + a1 + "<br>"+ a2+"<br>"+a3+"<br>"+a4)
+console.log(heading2 + a1)
